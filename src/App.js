0649ca94964299a50721b2866589a6f489f41285
Vivek Kumar <vivekkumarbeg7@gmail.com>
2024-03-22 16:51:16 +0530

    "test"

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import Card from './components/Card';
 
@@ -13,7 +13,7 @@ let newArr=[1,2,3,5]
     <div className="App">
       <header className='content-center'>
         <h1 className='bg-blue-300 text-black
-       p-4 rounded-xl mb-4'>Tailwind test hello
+       p-4 rounded-xl mb-4'>Tailwind test hello from vivek
         </h1>
         <div className='flex flex-wrap mr-4 content-center'>
           <Card productName="MacBook" btnText="buy"></Card>
